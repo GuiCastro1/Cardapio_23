02a84b6b51a0feb31e68e5d21de072927be25bda
Guilherme Castro <gui.castro.0001.1426@gmail.com>
2025-08-12 02:58:13 -0300

    teste de api

--- JS/plates.js
+++ JS/plates.js
@@ -1,44 +1,44 @@
-export const plates = () => {
-    const plates = [
-        {
-            name: "2a VIRADO AO 23",  
-            price: 39.80
-        },
-        {
-            name: "3a PICADINHO DE CARNE",  
-            price: 36.00
-        },
-        {
-            name: "4a FEIJOADA",  
-            price: 41.50
-        },
-        {
-            name: "5a MASSA",  
-            price: 35.00
-        },
-        {
-            name: "6a PEIXE",  
-            price: 38.00
-        },
-        {
-            name: "Sábado FEIJOADA",  
-            price: 41.50
-        },
+// export const plates = () => {
+//     const plates = [
+//         {
+//             name: "2a VIRADO AO 23",  
+//             price: 39.80
+//         },
+//         {
+//             name: "3a PICADINHO DE CARNE",  
+//             price: 36.00
+//         },
+//         {
+//             name: "4a FEIJOADA",  
+//             price: 41.50
+//         },
+//         {
+//             name: "5a MASSA",  
+//             price: 35.00
+//         },
+//         {
+//             name: "6a PEIXE",  
+//             price: 38.00
+//         },
+//         {
+//             name: "Sábado FEIJOADA",  
+//             price: 41.50
+//         },
         
-    ];
+//     ];
 
-    const divPlates = document.querySelector(".plates");
+//     const divPlates = document.querySelector(".plates");
 
-    const ul = document.createElement("ul");
+//     const ul = document.createElement("ul");
 
-    plates.forEach(plate => {
-        const li = document.createElement("li");
-        li.classList.add("plate");
-        li.innerHTML = `
-            <h5>${plate.name}</h5>
-            <p>R$ ${plate.price.toFixed(2).replace(".", ",")}</p>
-        `;
-        ul.appendChild(li);
-    });
-    divPlates.appendChild(ul);
-}
+//     plates.forEach(plate => {
+//         const li = document.createElement("li");
+//         li.classList.add("plate");
+//         li.innerHTML = `
+//             <h5>${plate.name}</h5>
+//             <p>R$ ${plate.price.toFixed(2).replace(".", ",")}</p>
+//         `;
+//         ul.appendChild(li);
+//     });
+//     divPlates.appendChild(ul);
+// }
